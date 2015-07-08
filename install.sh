142d139505e7e501e1aaab2437fb2c9e258e628e
Gerd Wachsmuth <gerd.wachsmuth@mathematik.tu-chemnitz.de>
2015-07-08 19:40:05 +0200

    Extended install script.

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
 if [ "x$HOST" = "xcantor" ]; then
-	cat Xresources solarized_xresources/Xresources.light > ../.Xresources
+	cat Xresources solarized_xresources/Xresources.light > "$HOME/.Xresources"
 else
-	cat Xresources solarized_xresources/Xresources.dark > ../.Xresources
+	cat Xresources solarized_xresources/Xresources.dark > "$HOME/.Xresources"
+fi
+
+cd "$HOME"
+
+RELPATH="$(relpath "$(dirname $0)")"
+
+for file in dircolors xsessionrc; do
+	ln -s "$RELPATH/$file" ".$file"
+done
+
+if [ "x$HOST" = "xcantor" ]; then
+	file=xinitrc
+	ln -s "$RELPATH/$file" ".$file"
 fi
