76ac070372d19ad9077bf6c19c08be92df3447c0
Gerd Wachsmuth <gerd.wachsmuth@mathematik.tu-chemnitz.de>
2017-04-21 20:16:53 +0200

    Use window_ret instead of CURRENTWINDOW
    Seems to be faster.

--- i3/src/i3_vim_focus.c
+++ i3/src/i3_vim_focus.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]) {
 				break;
 		}
 
-		xdo_send_keysequence_window(xdo, CURRENTWINDOW, cmd, 0);
+		xdo_send_keysequence_window(xdo, window_ret, cmd, 0);
 	}
 	else {
 		fprintf(stderr, "no VIM!\n");
